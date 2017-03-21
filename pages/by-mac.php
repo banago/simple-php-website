e72f5f38dfeb9ef77d5451c173f65d0064a0a627
rohscx <rohscx@gmail.com>
2017-03-21 17:53:24 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -38,7 +38,7 @@ if ($err) {
   echo "cURL Error #:" . $err;
 } else {
   $json = json_decode($response, true);
-   echo $json['queryResponse']['entity'][0]['@dtoType'];
+   echo $json['queryResponse']['entity'][0]['clientsDTO'];
   //print_r($json);
   //echo $response;
   //echo $json['vlanId']['associationTime'];
