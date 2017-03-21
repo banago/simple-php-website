14819f699154a86c7648dcb72d6bdc3ac02bcca4
rohscx <rohscx@gmail.com>
2017-03-21 13:54:40 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -3,11 +3,13 @@
 </p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 
 
+
+
+<?php 
 <form action="<?=$_SERVER['PHP_SELF'];?>" method="post">
     <input type="button" name="submit" value="Submit">
 </form>
 
-<?php 
 include 'restAuth.php';
 if(isset($_POST['submit'])) { 
     iseAuth(); 
