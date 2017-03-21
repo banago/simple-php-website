efed501843ad67a62da41572ca566c9a937f0170
rohscx <rohscx@gmail.com>
2017-03-21 13:41:02 -0400

    Update restAuth.php

--- restAuth.php
+++ restAuth.php
@@ -1,9 +1,9 @@
 <?php
 
 iseAuth array(
-    "authorization: Basic aaaaaaa",
-    "cache-control: no-cache",
-    "postman-token: 111111111"
-  );
+                "authorization: Basic aaaaaaa",
+                "cache-control: no-cache",
+                "postman-token: 111111111"
+                 );
 echo "aasdfasfd dsafasdfa  asdfasf";
 ?>
