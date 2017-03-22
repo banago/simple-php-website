e902683242f7a49fddc4f68141855eadf7d1cfdb
rohscx <rohscx@gmail.com>
2017-03-22 14:15:25 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -121,7 +121,12 @@ if ($err) {
 ?>
 
 	
-	
+ <html>
+       <head>
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script> 
+            <script src="http://malsup.github.com/jquery.form.js"></script> 
+	    <script tap="cats"></script>
+         <script>	
 	
 	
 <?php
