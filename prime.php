880a984a5f6a6d1b3dd23b0503d5af78777d185f
rohscx <rohscx@gmail.com>
2017-03-22 17:41:08 -0400

    Update prime.php

--- prime.php
+++ prime.php
@@ -1,5 +1,40 @@
 <?php
 $name = $_GET['name'];
 echo "Welcome to the website " . $name .'.';
+function primeMac($name)
+{
+  echo "This is the the VAR" . $name;
+  <?php
+    header('Content-Type: application/json');
+
+    $aResult = array();
+
+    if( !isset($_POST['functionname']) ) { $aResult['error'] = 'No function name!'; }
+
+    if( !isset($_POST['arguments']) ) { $aResult['error'] = 'No function arguments!'; }
+
+    if( !isset($aResult['error']) ) {
+
+        switch($_POST['functionname']) {
+            case 'add':
+               if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 2) ) {
+                   $aResult['error'] = 'Error in arguments!';
+               }
+               else {
+                   $aResult['result'] = add(floatval($_POST['arguments'][0]), floatval($_POST['arguments'][1]));
+               }
+               break;
+
+            default:
+               $aResult['error'] = 'Not found function '.$_POST['functionname'].'!';
+               break;
+        }
+
+    }
+
+    echo json_encode($aResult);
+
+?>
+}
 
 ?>
