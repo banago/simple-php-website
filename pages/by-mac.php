abcf628a806289918403b827858541064969c091
rohscx <rohscx@gmail.com>
2017-03-22 12:11:01 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -135,7 +135,7 @@ if ($err) {
                  $('#sub').click(function() {
                      var var_data = "Hello World";
                      $.ajax({
-                         url: 'http://10.16.3.238/?page=by-mac',
+                         url: '',
                          type: 'GET',
                           data: { var_PHP_data: var_data },
                           success: function(data) {
