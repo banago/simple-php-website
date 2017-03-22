adcdeadac2defe5eea0f03052be92269dce0c74b
rohscx <rohscx@gmail.com>
2017-03-22 18:53:10 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -54,7 +54,23 @@ function macformat1(){
 			xc = xb +ya + xb;
 			document.getElementById("macid1").innerHTML+= xc + "<br>";
 		}
-		window.location.href="prime.php?name=" + xc;
+		
+    		function reqListener () {
+      			console.log(this.responseText);
+    		}
+    		var oReq = new XMLHttpRequest(); //New request object
+    		oReq.onload = function() {
+        	//This is where you handle what to do with the response.
+        	//The actual data is found on this.responseText
+        	alert(this.responseText); //Will alert: 42
+    		};
+    		oReq.open("get", "prime.php?name="+xc, true);
+    		//                               ^ Don't block the rest of the execution.
+   		//                                 Don't wait until the request finishes to 
+    		//                                 continue.
+    		oReq.send();
+		
+		//window.location.href="prime.php?name=" + xc;
 	
 	}else {
 		y = 0;
