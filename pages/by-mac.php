5c05834fc3384ed8819d70a8d9f14b802719fa12
rohscx <rohscx@gmail.com>
2017-03-22 17:29:39 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -52,9 +52,24 @@ function macformat1(){
 				txtArray[i] = ya;
 			}
 			xc = xb +ya + xb;
-			yak = window.location.href="prime.php?name="+xc;
+			yak = window.location.href="function.php?name="+xc;
 			//document.getElementById("macid1").innerHTML+= xc + "<br>";
-			document.getElementById("macid1").innerHTML+= yak + "<br>"
+			//document.getElementById("macid1").innerHTML+= yak + "<br>"
+			jQuery.ajax({
+    				type: "GET",
+   			 	url: 'function.php',
+    				dataType: 'json',
+    				data: {functionname: 'add', arguments: [1, 2]},
+
+    				success: function (obj, textstatus) {
+                  			if( !('error' in obj) ) {
+                      				yourVariable = obj.result;
+					}
+                  else {
+                      console.log(obj.error);
+                  }
+            }
+});
 			
 		}
 	
