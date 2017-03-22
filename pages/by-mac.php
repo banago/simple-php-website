cc379e6ac236bb2b2254031d17db0c9953cf41e7
rohscx <rohscx@gmail.com>
2017-03-22 10:54:37 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -3,7 +3,7 @@
 </p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 
 
-<form method="POST">
+
 <div class="entry-content">
       
         
@@ -73,8 +73,7 @@ include 'restAuth.php';
 $macAddress = '(%2218%3A66%3Ada%3A10%3A9d%3A94%22)';
 echo $iseAddress . $macAddress . '<br/>';
 
-$blah = $_GET['xb'];
-echo "This is the value of blah" . $blah;
+echo "asdfasfdsa ".$_GET['xa']."<br>";
 	
 if (isset($_POST['button1'])) 
 { 
