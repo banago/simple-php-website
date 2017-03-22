8b218ac8b04e353980ea8fbf6a7bcf93ffca61bf
rohscx <rohscx@gmail.com>
2017-03-22 17:05:45 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -52,9 +52,9 @@ function macformat1(){
 				txtArray[i] = ya;
 			}
 			xc = xb +ya + xb;
-			yak = window.location.href=”prime.php?name="xc";
+			//yak = window.location.href=”prime.php?name="xc";
 			document.getElementById("macid1").innerHTML+= xc + "<br>";
-			document.getElementById("macid1").innerHTML+= yak + "<br>"
+			//document.getElementById("macid1").innerHTML+= yak + "<br>"
 			
 		}
 	
