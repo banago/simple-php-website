bf3d6340ff93b48c1410d609c076f2993e3f54d1
rohscx <rohscx@gmail.com>
2017-03-22 18:21:39 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -1,6 +1,6 @@
 <?php
 include 'restAuth.php';
-//include 'prime.php';
+include 'prime.php';
 ?>
 <p>This is the <b>By Mac</b> page. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 </p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
@@ -17,7 +17,6 @@ include 'restAuth.php';
 </form>
 <button onclick="macformat1()">Submit</button>
 <script>
-
 function macformat1(){
 	var x,y,ya,z,za,xa,xb,xc,bc1,be1,name,a,b,answer,sp,sp1,sp2,yak;
 	x=document.getElementById("form2");
@@ -28,7 +27,6 @@ function macformat1(){
 	xa = "%3A";
 	xb = "%22";
 	xc;
-
 	//document.getElementById("calcid1").innerHTML="NewMac "+ za + "<br>";
 	sp= [];
 	//replaces new lines with a space and splits based on space
@@ -53,7 +51,7 @@ function macformat1(){
 			}
 			xc = xb +ya + xb;
 			//yak = window.location.href="function.php?name="+xc;
-			document.getElementById("macid1").innerHTML+= xc + "<br>";
+			//document.getElementById("macid1").innerHTML+= xc + "<br>";
 			//document.getElementById("macid1").innerHTML+= yak + "<br>"
 	
                   else {
