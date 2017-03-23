25a438f68b5fe05f33c75d5b31cd5473e99f6729
rohscx <rohscx@gmail.com>
2017-03-22 20:12:19 -0400

    Update prime.php

--- prime.php
+++ prime.php
@@ -46,9 +46,9 @@ if ($err) {
 } else {
   //$json = json_encode($response);
   $json = json_decode($response, true);
-  $match = array('type','family','managementIpAddress')
+  $match = array('type','family','managementIpAddress');
   echo $json['response']['0']['family'] . "\r\n";
-  //echo print_r($json);
+  //echo print_r($json) . "\r\n";
   for ($i1 = 0; $i1 < count($match); $i1++) {
     for ($i2 = 0; $i2 < count($json); $i2++) {
       echo $json['response'][$i1][$i2] . "\r\n";
