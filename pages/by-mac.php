0418615b8b06c40bd2aa3f9856af2d77aeff332b
rohscx <rohscx@gmail.com>
2017-03-23 13:02:42 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -85,8 +85,23 @@ echo $iseAddress . $macAddress;
 <?php
 if (isset($_POST['name'])) 
 { 
-	$mac = $_POST['name'];
-	iseMAC($macAddress);
+	$xc = $_POST['name'];
+	//$mac = $_POST['name'];
+	//iseMAC($macAddress);
+	function reqListener () {
+		console.log(this.responseText);
+	}
+	var oReq = new XMLHttpRequest(); //New request object
+    	oReq.onload = function() {
+        //This is where you handle what to do with the response.
+        //The actual data is found on this.responseText
+        alert(this.responseText); //Will alert: 42
+    	};
+	oReq.open("get", "isefunctions.php?name="+xc, true);
+    	//                               ^ Don't block the rest of the execution.
+   	//                                 Don't wait until the request finishes to 
+    	//                                 continue.
+    	oReq.send();
 }
 ?>
 
