50c331726fcd14c480d2c8967d33cc32132cbd93
rohscx <rohscx@gmail.com>
2017-03-23 10:38:56 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -1,5 +1,3 @@
-
-
 <p>This is the <b>By Mac</b> page. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 </p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 
@@ -49,7 +47,9 @@ function macformat1(){
 			}
 			xc = xb +ya + xb;
 			document.getElementById("macid1").innerHTML+= xc + "<br>";
-			    		function reqListener () {
+		}
+		
+    		function reqListener () {
       			console.log(this.responseText);
     		}
     		var oReq = new XMLHttpRequest(); //New request object
@@ -63,10 +63,9 @@ function macformat1(){
    		//                                 Don't wait until the request finishes to 
     		//                                 continue.
     		oReq.send();
-		}
 		
-
-
+		//window.location.href="prime.php?name=" + xc;
+	
 	}else {
 		y = 0;
 	}
@@ -74,5 +73,3 @@ function macformat1(){
 </script>
 <p id="macid1"></p>
 </div></div></div></div></div></div>
-
-	
