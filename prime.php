141b4c6a5f7d6bdb8920bd0d8cc9e4767dbc438f
rohscx <rohscx@gmail.com>
2017-03-23 09:22:48 -0400

    Update prime.php

--- prime.php
+++ prime.php
@@ -14,57 +14,56 @@ echo json_encode(42) . "\r\n"; //In the end, you need to echo the result.
                       //even objects.
 if (isset($_GET['primeMac'])) 
 {
-$name = $_GET['primeMac'];
-echo "This mac was passed" . $name . "\r\n";
-
-
-$macAddress = '(%2218%3A66%3Ada%3A10%3A9d%3A94%22)';
-echo $iseAddress . $macAddress . "\r\n";
-	
-
-   
-   echo "button 1 has been pressed" . "\r\n"; 
-   $getthevalueofid = id;
-   echo $getthevalueofid. "\r\n";
-   //iseAuth();
-   $curl = curl_init();
-   $somevar = $_GET["uid"];
-   curl_setopt_array($curl, array(
-      CURLOPT_SSL_VERIFYPEER => false,
-      CURLOPT_SSL_VERIFYHOST => false,
-      CURLOPT_URL => $primeAddress . $macAddress,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 300,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => $primeAuth,
-));
-$response = curl_exec($curl);
-$err = curl_error($curl);
-curl_close($curl);
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  $json = json_decode($response, true);
-  //print_r($json);
-  //echo $response;
-  //echo $json['vlanId']['associationTime'];
-	$match = array('connectionType','deviceIpAddress','deviceName','deviceType','hostname','ipAddress','macAddress','securityPolicyStatus','userName');
-	echo $json['queryResponse']['entity']['0']['clientsDTO']['securityPolicyStatus'] . "\r\n";
-  	//echo print_r($json) . "\r\n";
-	$count = count($json)['queryResponse']['entity']['0']['clientsDTO'];
-	echo "Array contains this many values ::" . $count . "\r\n";
- 	for ($i1 = 0; $i1 < count($json['queryResponse']['entity']['0']); $i1++) {
-		//$count = count($json)['response']['entity']['0']['clientsDTO'];
-		//echo "Array contains this many values ::" . $count . "\r\n";
-    		foreach ($match as $item) 
+	function writeMsg() 
+	{
+		echo "Hello world!";
+		$name = $_GET['primeMac'];
+		echo "This mac was passed" . $name . "\r\n";
+		$macAddress = '(%2218%3A66%3Ada%3A10%3A9d%3A94%22)';
+		echo $iseAddress . $macAddress . "\r\n";
+	  	$getthevalueofid = id;
+  		echo $getthevalueofid. "\r\n";
+     		$curl = curl_init();
+   		$somevar = $_GET["uid"];
+   		curl_setopt_array($curl, array(
+      			CURLOPT_SSL_VERIFYPEER => false,
+      			CURLOPT_SSL_VERIFYHOST => false,
+      			CURLOPT_URL => $primeAddress . $macAddress,
+      			CURLOPT_RETURNTRANSFER => true,
+     	 		CURLOPT_ENCODING => "",
+      			CURLOPT_MAXREDIRS => 10,
+      			CURLOPT_TIMEOUT => 300,
+      			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      			CURLOPT_CUSTOMREQUEST => "GET",
+      			CURLOPT_HTTPHEADER => $primeAuth,
+		));
+		
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+		curl_close($curl);
+		if ($err) 
 		{
+  			echo "cURL Error #:" . $err;
+		} else {
+			$json = json_decode($response, true);
+  			//print_r($json);
+  			//echo $response;
+ 	 		//echo $json['vlanId']['associationTime'];
+			$match = array('connectionType','deviceIpAddress','deviceName','deviceType','hostname','ipAddress','macAddress','securityPolicyStatus','userName');
+			echo $json['queryResponse']['entity']['0']['clientsDTO']['securityPolicyStatus'] . "\r\n";
+  			//echo print_r($json) . "\r\n";
+			$count = count($json)['queryResponse']['entity']['0']['clientsDTO'];
+			echo "Array contains this many values ::" . $count . "\r\n";
+ 			for ($i1 = 0; $i1 < count($json['queryResponse']['entity']['0']); $i1++) {
+				//$count = count($json)['response']['entity']['0']['clientsDTO'];
+				//echo "Array contains this many values ::" . $count . "\r\n";
+    				foreach ($match as $item) 
+			{
 			echo $json['queryResponse']['entity'][$i1]['clientsDTO'][$item] . "\r\n";
+		
+				}
+			}
 		}
 	}
 }
-}
-
 ?>
