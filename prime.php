19ce76eac7fe44981282697fe3cde33acc4f70ca
rohscx <rohscx@gmail.com>
2017-03-22 20:25:49 -0400

    Update prime.php

--- prime.php
+++ prime.php
@@ -49,9 +49,10 @@ if ($err) {
   $match = array('type','family','managementIpAddress');
   echo $json['response']['0']['family'] . "\r\n";
   echo print_r($json) . "\r\n";
-  for ($i1 = 0; $i1 < count($match); $i1++) {
-    for ($i2 = 0; $i2 < count($json); $i2++) {
-      echo $json['response'][$i2][$match[$i1]] . "\r\n";
+  for ($i1 = 0; $i1 < count($json); $i1++) {
+    echo $json['response'][$i1][$match[$i1]] . "\r\n";
+    for ($i2 = 0; $i2 < count($match); $i2++) {
+      //echo $json['response'][$i2][$match[$i1]] . "\r\n";
       //echo $i2 . $match[$i1] . "\r\n";
     }
     }
