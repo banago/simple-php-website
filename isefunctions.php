05070e83707605aa6a30f56e24ac4ccd4d36bb21
rohscx <rohscx@gmail.com>
2017-03-23 12:57:14 -0400

    Update isefunctions.php

--- isefunctions.php
+++ isefunctions.php
@@ -1,5 +1,14 @@
 <?php
-
+/* Do some operation here, like talk to the database, the file-session
+ * The world beyond, limbo, the city of shimmers, and Canada.
+ * 
+ * AJAX generally uses strings, but you can output JSON, HTML and XML as well. 
+ * It all depends on the Content-type header that you send with your AJAX
+ * request. */
+echo json_encode(42) . "\r\n"; //In the end, you need to echo the result. 
+                      //All data should be json_encode()d.
+                      //You can json_encode() any value in PHP, arrays, strings,
+                      //even objects.
 function iseMAC($macAddress){
   include 'restAuth.php';
   echo "Hello world!";
@@ -45,4 +54,5 @@ function iseMAC($macAddress){
 	}
 }
 }
+
 ?>
