3597e8a0a34bd446e93a9c9c9692698d86e9d326
rohscx <rohscx@gmail.com>
2017-03-29 09:52:40 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -62,7 +62,7 @@ function macformat1(){
         	//The actual data is found on this.responseText
         	alert(this.responseText); //Will alert: 42
     		};
-    		oReq.open("get", "isefunctions.php?iseMac="+ya, true);
+    		oReq.open("get", "isefunctions.php?iseMAC="+ya, true);
     		//                               ^ Don't block the rest of the execution.
    		//                                 Don't wait until the request finishes to 
     		//                                 continue.
