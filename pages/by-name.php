7fd2d89a1e6acb2e04f2f12a00c37c3d1fe183c4
rohscx <rohscx@gmail.com>
2017-03-30 18:35:10 -0400

    Update by-name.php

--- pages/by-name.php
+++ pages/by-name.php
@@ -96,3 +96,30 @@ window.onclick = function(event) {
 }
 </script>
 
+<?php
+
+$request = new HttpRequest();
+$request->setUrl('https://devnetapi.cisco.com/sandbox/apic_em/api/v1/network-device');
+$request->setMethod(HTTP_METH_GET);
+
+$request->setHeaders(array(
+  'postman-token' => '6a30ecb5-e42e-46e2-e88e-26ddf9c7f11d',
+  'cache-control' => 'no-cache',
+  'x-auth-token' => 'ST-1974-59fqSJ0BOl24oDtf0Nmx-cas',
+  'content-type' => 'application/json'
+));
+
+$request->setBody('{"username":"devnetuser",
+"password":"Cisco123!"
+}');
+
+try {
+  $response = $request->send();
+
+  echo $response->getBody();
+} catch (HttpException $ex) {
+  echo $ex;
+}
+    
+    ?>
+    
