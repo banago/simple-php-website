75ffa61d90d1af2b65f382fbf210895b130714ec
rohscx <rohscx@gmail.com>
2017-03-30 18:39:04 -0400

    Update by-name.php

--- pages/by-name.php
+++ pages/by-name.php
@@ -98,27 +98,33 @@ window.onclick = function(event) {
 
 <?php
 
-$request = new HttpRequest();
-$request->setUrl('https://devnetapi.cisco.com/sandbox/apic_em/api/v1/network-device');
-$request->setMethod(HTTP_METH_GET);
+$curl = curl_init();
 
-$request->setHeaders(array(
-  'postman-token' => 'bb698b54-d89e-00e2-2c1d-5a0713397277',
-  'cache-control' => 'no-cache',
-  'x-auth-token' => 'ST-1989-tESpJnucQ7xwwVdB3f9w-cas',
-  'content-type' => 'application/json'
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://devnetapi.cisco.com/sandbox/apic_em/api/v1/network-device",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_POSTFIELDS => "{\"username\":\"devnetuser\",\n\"password\":\"Cisco123!\"\n}",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache",
+    "content-type: application/json",
+    "postman-token: f36c170e-54a8-8412-eabe-5656b6229dfb",
+    "x-auth-token: ST-1989-tESpJnucQ7xwwVdB3f9w-cas"
+  ),
 ));
 
-$request->setBody('{"username":"devnetuser",
-"password":"Cisco123!"
-}');
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-try {
-  $response = $request->send();
+curl_close($curl);
 
-  echo $response->getBody();
-} catch (HttpException $ex) {
-  echo $ex;
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
 }
-    
-    ?>
+?>
