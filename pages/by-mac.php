9602b732a5722f15076fdf65a635f09de13ac3b2
rohscx <rohscx@gmail.com>
2017-03-30 14:20:11 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -94,7 +94,7 @@ function macformat1(){
 		//window.location.href="prime.php?name=" + xc;
 		oReq.send();
 		document.getElementById("macid1").innerHTML+= xc + "<br>";
-		 $('.spinner');
+		document.getElementById("macid1").innerHTML+="<div" + "class=" + "\"spinner\">" + "</div>";
 	}else {
 		y = 0;
 	}
