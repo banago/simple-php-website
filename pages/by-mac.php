099c20d45302cdf7ed30793ef3c74a9b9c50e30a
rohscx <rohscx@gmail.com>
2017-03-30 11:40:21 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -17,8 +17,8 @@ include 'isefunctions.php';
 		  
 <style>		  
 .loader {
-    border: 16px solid #f3f3f3; /* Light grey */
-    border-top: 16px solid #3498db; /* Blue */
+    border: 4px solid #f3f3f3; /* Light grey */
+    border-top: 4px solid #3498db; /* Blue */
     border-radius: 50%;
     width: 15px;
     height: 15px;
