f83ba4baf50bb6cd4f82c01ca0c81980ceabab85
rohscx <rohscx@gmail.com>
2017-03-30 13:55:21 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -94,7 +94,7 @@ function macformat1(){
 		//window.location.href="prime.php?name=" + xc;
 		oReq.send();
 		document.getElementById("macid1").innerHTML+= xc + "<br>";
-		document.writeln("<tr><td class="spinner">" + Display + "</td></tr>");
+		spinner;
 	}else {
 		y = 0;
 	}
