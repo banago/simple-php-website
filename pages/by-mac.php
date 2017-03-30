ff667654d3877e98fc6cee51b486ba9e02e9567d
rohscx <rohscx@gmail.com>
2017-03-30 11:38:44 -0400

    Update by-mac.php

--- pages/by-mac.php
+++ pages/by-mac.php
@@ -20,8 +20,8 @@ include 'isefunctions.php';
     border: 16px solid #f3f3f3; /* Light grey */
     border-top: 16px solid #3498db; /* Blue */
     border-radius: 50%;
-    width: 15px;
-    height: 30px;
+    width: 30px;
+    height: 15px;
     animation: spin 2s linear infinite;
 }
 
