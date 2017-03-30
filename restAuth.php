72dba91c3c82043222e74be6b7a25a75bf514fb0
rohscx <rohscx@gmail.com>
2017-03-30 18:33:26 -0400

    Update restAuth.php

--- restAuth.php
+++ restAuth.php
@@ -1,34 +1,28 @@
 <?php
   function first($int, $string){ //function parameters, two variables.
-    return $string;  //returns the second argument passed into the function
-    $restAddress = "https://devnetapi.cisco.com/sandbox/apic_em/api/v1";
-    $restCall = "/network-device";
-    $restHeader ="ST-1974-59fqSJ0BOl24oDtf0Nmx-cas";
-    $curl = curl_init();
-    $somevar = $_GET["uid"];
-    curl_setopt_array($curl, array(
-      CURLOPT_SSL_VERIFYPEER => false,
-      CURLOPT_SSL_VERIFYHOST => false,
-      CURLOPT_URL => $restAddress . $restCall,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 300,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => array(
-    "cache-control: no-cache",
-    "content-type: application/json",
-    "postman-token: c94ec74d-61d0-0351-d58a-e3785abb63fd",
-    "x-auth-token: ST-1974-59fqSJ0BOl24oDtf0Nmx-cas"
-      ),
-    ));
-    curl_close($curl);
-    if ($err) {
-      echo "cURL Error #:" . $err;
-    } else {
-      $json = json_decode($response, true);
-      print_r($json);
-    }
+    
+$request = new HttpRequest();
+$request->setUrl('https://devnetapi.cisco.com/sandbox/apic_em/api/v1/network-device');
+$request->setMethod(HTTP_METH_GET);
+
+$request->setHeaders(array(
+  'postman-token' => '6a30ecb5-e42e-46e2-e88e-26ddf9c7f11d',
+  'cache-control' => 'no-cache',
+  'x-auth-token' => 'ST-1974-59fqSJ0BOl24oDtf0Nmx-cas',
+  'content-type' => 'application/json'
+));
+
+$request->setBody('{"username":"devnetuser",
+"password":"Cisco123!"
+}');
+
+try {
+  $response = $request->send();
+
+  echo $response->getBody();
+} catch (HttpException $ex) {
+  echo $ex;
+}
+
   }
 ?>
