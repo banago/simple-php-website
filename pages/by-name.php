24d140a87e307c4409991126c69d3ca19cdd2ca6
rohscx <rohscx@gmail.com>
2017-03-30 20:42:57 -0400

    Update by-name.php

--- pages/by-name.php
+++ pages/by-name.php
@@ -13,7 +13,7 @@
     left: 0;
     top: 0;
     width: 100%; /* Full width */
-    height: 200%; /* Full height */
+    height: 100%; /* Full height */
     overflow: auto; /* Enable scroll if needed */
     background-color: rgb(0,0,0); /* Fallback color */
     background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
