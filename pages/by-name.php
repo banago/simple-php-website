911cb9bff413abd82829d8db9503da081199dfbb
rohscx <rohscx@gmail.com>
2017-03-30 22:28:24 -0400

    Update by-name.php

--- pages/by-name.php
+++ pages/by-name.php
@@ -94,9 +94,10 @@
       <p>GET Result</p>
       <?php
       include 'restAuth.php';
-      $ticket = apicTicket_1();
+      //Debug  
+      //$ticket = apicTicket_1();
       echo apicTicket_1();
-      echo apicRest_1($ticket); //returns Apic results;
+      echo apicRest_1(apicTicket_1()); //returns Apic results;
       ?>
     </div>
     <div class="modal-footer">
