120c3fe0d1610cb4b3c068d4f94b662742f5daca
rohscx <rohscx@gmail.com>
2017-03-30 22:10:16 -0400

    Update by-name.php

--- pages/by-name.php
+++ pages/by-name.php
@@ -1,30 +1,6 @@
 <p>This 1is <b>By Name</b> page. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 </p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 
-<?php
-
-$request = new HttpRequest();
-$request->setUrl('https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket');
-$request->setMethod(HTTP_METH_POST);
-
-$request->setHeaders(array(
-  'postman-token' => '97142a82-74ae-9225-255a-2d099d78a9ba',
-  'cache-control' => 'no-cache',
-  'content-type' => 'application/json'
-));
-
-$request->setBody('{"username":"devnetuser",
-"password":"Cisco123!"
-}');
-
-try {
-  $response = $request->send();
-
-  echo $response->getBody();
-} catch (HttpException $ex) {
-  echo $ex;
-}
-?>
 
 <style>
 /* The Modal (background) */
@@ -118,6 +94,7 @@ try {
       <p>GET Result</p>
       <?php
       include 'restAuth.php';
+      echo apicTicket_1();
       echo apicRest_1(); //returns Apic results;
       ?>
     </div>
