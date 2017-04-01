f2b816b219ef746c3348e21436f8006a38677887
rohscx <rohscx@gmail.com>
2017-04-01 14:11:06 -0400

    Update restAuth.php

--- restAuth.php
+++ restAuth.php
@@ -165,3 +165,46 @@ function primeTicket_1(){
 	}
 }
 ?>
+
+<?php
+function deviceURL_1(){
+	$str = "192.1 68.1.1";
+	$str = preg_replace('/\s+/', '', $str);     //Remove whitespaces
+	$str_1 = (preg_match('/^([0-9A-Fa-f]{2}[:\-\.]){5}([0-9A-Fa-f]{2})$/', $str) == 1);
+	$str_2 = (preg_match('/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/', $str) == 1);
+	if ($str_1 == 1) {
+		echo "It's a mac!!!";
+    	//make uppper
+    	$str = strtoupper($str);
+    	//list diliminators
+    	$separator = array(':', '-','.');
+    	//strip dilimintors
+    	$str = str_replace($separator, '', $str);
+   		//split by 2
+    	$split_2 = str_split($str, 2);
+    	//Debug;
+    	//$count = count($split_2);
+	    //insert new diliinator
+    	//$mac_dashes = implode('-', $split_2);         // A0-B0-C0-D0-E0-F0
+	    $mac_colons = implode(':', $split_2);           // A0:B0:C0:D0:E0:F0
+	    $add_quotes = "\"" .$mac_colons . "\"";         // "A0:B0:C0:D0:E0:F0"
+	    $url_encode = urlencode($add_quotes);           // %2208%3AE8%3A56%3A40%3AF4%3A48%22
+	    //echo $str;
+	    //echo $mac_colons;
+	    //echo $mac_dashes;
+	    echo $url_encode;
+	    return $url_encode;
+} elseif ($str_2 == 1) {
+	echo "it's a ip!!!";
+    $add_quotes = "\"" .$str. "\"";                 // "192.168.1.1"
+    $url_encode = urlencode($add_quotes);           // %22192.168.1.1%22
+    echo $url_encode;
+    return $url_encode;
+} else { 
+    echo "it's probably a hostname!!!";
+    $add_quotes = "\"" .$str. "\"";                 // "macbook_123.user.net"
+    $url_encode = urlencode($add_quotes);           // %22macbook_123.user.net%22
+    echo $url_encode;
+    return $url_encode;
+}	
+?>
