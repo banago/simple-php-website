e5573439a5bea8e1a9e640b22df7ec049c4b0488
rohscx <rohscx@gmail.com>
2017-04-02 10:50:41 -0400

    Update by-name.php

--- pages/by-name.php
+++ pages/by-name.php
@@ -138,23 +138,18 @@ MAC | IP | HOSTNAME : <input type="text" id="myData"> <br/>
 <script>
 // Get the modal
 var modal = document.getElementById('myModal');
-
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  
     modal.style.display = "block";
 }
-
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
@@ -162,4 +157,3 @@ window.onclick = function(event) {
     }
 }
 </script>
-
