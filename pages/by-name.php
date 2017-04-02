42c4858a6100c173fd4181bf94f96cff07be7114
rohscx <rohscx@gmail.com>
2017-04-02 14:59:03 -0400

    Update by-name.php

--- pages/by-name.php
+++ pages/by-name.php
@@ -107,27 +107,7 @@ xmlhttp.open('GET', thefile+'?'+thekey+'='+document.search.data_text.value, true
 xmlhttp.send();
 }
 
-// Get the modal
-var modal = document.getElementById('myModal');
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    document.getElementById("adiv2").innerHTML = "Hello World";
-    modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+
 </script>    
 </head>
 <body>
@@ -180,5 +160,25 @@ MAC | IP | HOSTNAME : <input type="text" name="data_text" onkeyup="findformat('a
 </div>
 
 <script>
-
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    document.getElementById("adiv2").innerHTML = "Hello World";
+    modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 </script>
