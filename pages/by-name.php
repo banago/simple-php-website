5bc7e0ae830e12d583264e23b90df00253c4bb66
rohscx <rohscx@gmail.com>
2017-04-02 10:37:25 -0400

    Update by-name.php

--- pages/by-name.php
+++ pages/by-name.php
@@ -76,18 +76,7 @@
 <head>
 <script type="text/javascript">
 function load(thediv, thefile) {
-    if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest();
-    } else { 
-        xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
-    }  
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readySate == 4 && xmlhttp.status == 200) {
-            document.getElementById(thediv).innerHTML = xmlhttp.responseText; 
-        }
-    }
-xmlhttp.open('GET', thefile, true);
-xmlhttp.send();
+alert('Works.');
 }
   
 </script>    
