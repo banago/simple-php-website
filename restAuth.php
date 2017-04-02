a66844915ea9f12f6892b55e36337a795c47a744
rohscx <rohscx@gmail.com>
2017-04-02 19:03:24 -0400

    Update restAuth.php

--- restAuth.php
+++ restAuth.php
@@ -47,52 +47,55 @@ function apicRest_1($ticket){ //function parameters, one variable
 	}  
 }
 
-function primeRest_1($ticket){ //function parameters, one variable
-	//Debug
-	//echo "Ticket : " . $ticket;
-	$curl = curl_init();
-	curl_setopt_array($curl, array(
-		CURLOPT_URL => "https://devnetapi.cisco.com/sandbox/apic_em/api/v1/network-device",
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_ENCODING => "",
-		CURLOPT_MAXREDIRS => 10,
-		CURLOPT_TIMEOUT => 30,
-		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		CURLOPT_CUSTOMREQUEST => "GET",
-		CURLOPT_POSTFIELDS => "{\"username\":\"devnetuser\",\n\"password\":\"Cisco123!\"\n}",
-		CURLOPT_HTTPHEADER => array(
-			"cache-control: no-cache",
-			"content-type: application/json",
-			"x-auth-token: " . $ticket
-			),
-		));
-	$response = curl_exec($curl);
-	$err = curl_error($curl);
-	curl_close($curl);
-	if ($err) {
-		echo "cURL Error #:" . $err;
-	} else {
+if (isset($_GET['apic_request'])) {
+	function primeRest_1($ticket){ //function parameters, one variable
 		//Debug
-		//echo $response;
-		$json = json_decode($response, true);
-		$match = array("Serial Number :"=>'serialNumber',"Family :"=>'family',"Type :"=>'type',
+		//echo "Ticket : " . $ticket;
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => "https://devnetapi.cisco.com/sandbox/apic_em/api/v1/network-device",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "GET",
+			CURLOPT_POSTFIELDS => "{\"username\":\"devnetuser\",\n\"password\":\"Cisco123!\"\n}",
+			CURLOPT_HTTPHEADER => array(
+				"cache-control: no-cache",
+				"content-type: application/json",
+				"x-auth-token: " . $ticket
+				),
+			));
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+		curl_close($curl);
+		if ($err) {
+			echo "cURL Error #:" . $err;
+		} else {
+			//Debug
+			//echo $response;
+			$json = json_decode($response, true);
+			$match = array("Serial Number :"=>'serialNumber',"Family :"=>'family',"Type :"=>'type',
 			       "Inventory :"=>'inventoryStatusDetail',"MAC :"=>'macAddress',
 			       "Role :"=>'role',"MgmT :"=>'managementIpAddress',
 			       "Platform :"=>'platformId',"Reachablity :"=>'reachabilityStatus',
 			       "HostName :"=>'hostname');
-		for ($i = 0; $i < count($json['response']); $i++) {
-			//Debug
-			//echo "How many response: " . count($json['response']) . "<br>";
-			echo "<br>";
-			echo "Item Count: " . $i . "<br>";
-			echo "<br>";
-			foreach ($match as $x => $item) {
-				echo $x ."  " . $json['response'][$i][$item] . "<br>";
+			for ($i = 0; $i < count($json['response']); $i++) {
+				//Debug
+				//echo "How many response: " . count($json['response']) . "<br>";
+				echo "<br>";
+				echo "Item Count: " . $i . "<br>";
+				echo "<br>";
+				foreach ($match as $x => $item) {
+					echo $x ."  " . $json['response'][$i][$item] . "<br>";
+				}
 			}
-		}
-		//Debug
-		//print_r($json);
-	}  
+			//Debug
+			//print_r($json);
+		}  
+	}
+	primeRest_1($_GET['apic_request']);
 }
 
 if (isset($_GET['get_ticket'])) {
