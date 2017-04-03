8e8429d6853659a63c1bef7ad9855b495897494b
rohscx <rohscx@gmail.com>
2017-04-03 10:54:42 -0400

    Update by-dev-apic.php

--- pages/by-dev-apic.php
+++ pages/by-dev-apic.php
@@ -35,7 +35,7 @@
     margin: auto;
     padding: 0;
     border: 1px solid #888;
-    width: 60%;
+    width: 30%;
     box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
     -webkit-animation-name: animatetop;
     -webkit-animation-duration: 0.4s;
