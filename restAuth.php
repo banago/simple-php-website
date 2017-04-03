c6a933e35de71221561de5171cbf9111ef45f077
rohscx <rohscx@gmail.com>
2017-04-03 09:07:30 -0400

    Update restAuth.php

--- restAuth.php
+++ restAuth.php
@@ -100,7 +100,7 @@ if (isset($_GET['use_ticket'])) {
 }
 
 if (isset($_GET['get_ticket'])) {
-	function primeTicket_1(){
+	function apicTicket_1(){
 		$curl = curl_init();
 		curl_setopt_array($curl, array(
 			CURLOPT_URL => "https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket",
@@ -131,41 +131,9 @@ if (isset($_GET['get_ticket'])) {
 			echo $response;
 		}
 	}
-	primeTicket_1();
+	apicTicket_1();
 }
 	
-if (isset($_GET['apic_response'])) {	
-	function primeTicket_1(){
-		$curl = curl_init();
-		curl_setopt_array($curl, array(
-			CURLOPT_URL => "https://devnetapi.cisco.com/sandbox/apic_em/api/v1/ticket",
-  			CURLOPT_RETURNTRANSFER => true,
-  			CURLOPT_ENCODING => "",
-  			CURLOPT_MAXREDIRS => 10,
-  			CURLOPT_TIMEOUT => 30,
-  			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  			CURLOPT_CUSTOMREQUEST => "POST",
-  			CURLOPT_POSTFIELDS => "{\"username\":\"devnetuser\",\n\"password\":\"Cisco123!\"\n}",
-  			CURLOPT_HTTPHEADER => array(
-				"cache-control: no-cache",
-    			"content-type: application/json",
-			),
-		));
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
 
-		curl_close($curl);
-
-		if ($err) {
-			echo "cURL Error #:" . $err;
-			} else {
-			//echo $response;
-			$json = json_decode($response, true);
-			//Debug
-			//print_r($json);
-			echo $response;
-		}
-	}
-}
 ?>
 
