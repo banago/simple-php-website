8732f5d6168792ade9d818a0386155b949e32244
rohscx <rohscx@gmail.com>
2017-04-04 16:19:17 -0400

    Update prime.php

--- prime.php
+++ prime.php
@@ -1,17 +1,4 @@
 <?php
-include 'restAuth.php';
-/* Do some operation here, like talk to the database, the file-session
- * The world beyond, limbo, the city of shimmers, and Canada.
- * 
- * AJAX generally uses strings, but you can output JSON, HTML and XML as well. 
- * It all depends on the Content-type header that you send with your AJAX
- * request. */
-
-echo json_encode(42) . "\r\n"; //In the end, you need to echo the result. 
-                      //All data should be json_encode()d.
-
-                      //You can json_encode() any value in PHP, arrays, strings,
-                      //even objects.
 if (isset($_GET['primeData']) & isset($_GET['primeAddress']))
 {
 $data = $_GET['primeData'];
