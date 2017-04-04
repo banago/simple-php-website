731053cb45d56aac4730c16bf83a571d6e8d7a54
rohscx <rohscx@gmail.com>
2017-04-04 16:44:05 -0400

    Update by-dev-prime.php

--- pages/by-dev-prime.php
+++ pages/by-dev-prime.php
@@ -114,7 +114,7 @@ function encoded_1(thediv, thefile, thekey) {
 	    }
 		else {
 		    document.getElementById('spinner').style.display = "none";
-		    document.getElementById(thediv).innerHTML = "Unfortunately this application only supports "+str.bold("MAC ")+"and "+str.bold("IP ")+"addresses";
+		    document.getElementById(thediv).innerHTML = "Unfortunately this application only supports ;
 	    }     
         }
     } 
