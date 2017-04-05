4bd47c47841e9913e810d2321305dba5a0245f1b
rohscx <rohscx@gmail.com>
2017-04-04 21:28:04 -0400

    Update prime.php

--- prime.php
+++ prime.php
@@ -2,11 +2,11 @@
 include 'restAuth.php';    // tickets tokons and secure data
 if (isset($_GET['primeData']) & isset($_GET['primeAddress']))
 {
-$data = $_GET['primeData'];
-$addr = $_GET['primeAddress'];
-//echo "This mac was passed" . $name . "\r\n";  // debug
-$data = "(" . $data . ")";
-// echo $addr . $data . "\r\n"; //debug
+   $data = $_GET['primeData'];
+   $addr = $_GET['primeAddress'];
+   // echo "This mac was passed" . $name . "\r\n";  // debug
+   $data = "(" . $data . ")";
+   // echo $addr . $data . "\r\n"; //debug
    $curl = curl_init();
    $somevar = $_GET["uid"];
    curl_setopt_array($curl, array(
@@ -22,35 +22,36 @@ $data = "(" . $data . ")";
       CURLOPT_HTTPHEADER => $primeAuth, //restAuth contains the auth Tokens. This also need to be update to return JSON instead of include
 ));
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
-curl_close($curl);
-if ($err) {
-  echo "cURL Error #:" . $err;
-} elseif ($array['http-code'] != 200) { 
-  echo print_r($array);
-} else {
-        $json = json_decode($response, true);
-        //print_r($json);  // debug
-        //echo $response;  // debug
-        //echo $json['vlanId']['associationTime']; // debug
-        $match = array("NAS Interface :"=>'clientInterface',"NAS Connection Type :"=>'connectionType',
-                                   "NAS IP :"=>'deviceIpAddress',"NAS Name :"=>'deviceName',
-                                   "Device Type :"=>'deviceType',"Device IP :"=>'ipAddress',
-                                   "MAC Address :"=>'macAddress',"Device Security Policy :"=>'securityPolicyStatus',
-                                   "Device OUI :"=>'vendor',"Device VLAN:"=>'vlan');
-    //echo $json['queryResponse']['entity']['0']['clientsDTO']['securityPolicyStatus'] . "\r\n";   // debug
-    //echo print_r($json) . "\r\n";    // debug
-        for ($i = 0; $i < count($json['queryResponse']['entity']); $i++) {
-                //Debug
-                //echo "How many response: " . count($json['response']) . "<br>";
-                echo "<br>";
-                echo "Array Element: " . $i . "<br>";
-                echo "<br>";
-                foreach ($match as $x => $item) {
-                        echo $x ."  " . $json['queryResponse']['entity']['0']['clientsDTO'][$item] . "<br>";
-                }
-        }
+   $response = curl_exec($curl);
+   $err = curl_error($curl);
+   curl_close($curl);
+   if ($err) {
+      echo "cURL Error #:" . $err;
+   } elseif ($array['http-code'] != 200) { 
+      echo print_r($array);
+   } else {
+      $json = json_decode($response, true);
+      //print_r($json);  // debug
+      //echo $response;  // debug
+      //echo $json['vlanId']['associationTime']; // debug
+      $match = array("NAS Interface :"=>'clientInterface',"NAS Connection Type :"=>'connectionType',
+                      "NAS IP :"=>'deviceIpAddress',"NAS Name :"=>'deviceName',
+                      "Device Type :"=>'deviceType',"Device IP :"=>'ipAddress',
+                      "MAC Address :"=>'macAddress',"Device Security Policy :"=>'securityPolicyStatus',
+                      "Device OUI :"=>'vendor',"Device VLAN:"=>'vlan');
+      //echo $json['queryResponse']['entity']['0']['clientsDTO']['securityPolicyStatus'] . "\r\n";   // debug
+      //echo print_r($json) . "\r\n";    // debug
+      for ($i = 0; $i < count($json['queryResponse']['entity']); $i++) {
+         //Debug
+         //echo "How many response: " . count($json['response']) . "<br>";
+         echo "<br>";
+         echo "Array Element: " . $i . "<br>";
+         echo "<br>";
+         foreach ($match as $x => $item) {
+            echo $x ."  " . $json['queryResponse']['entity']['0']['clientsDTO'][$item] . "<br>";
+         }
+      }
+   }   
 }
 }
 ?>
