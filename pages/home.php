05d1cda9ac727ad37e460b31b0b01601d03d67df
rohscx <rohscx@gmail.com>
2017-04-06 13:43:00 -0400

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -1,2 +1,3 @@
-<p>This is the <b>home</b> page.</p>
-<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+<p>Welcome to the <b>home</b> page.</p>
+<p>This is the initial release of the portal, updates, and changes will be included to increase functionality at a later date.</p>
+<p>The purpose of these tools is to provide insight into the operations of particular systems in a more easily absorbed format.</p>
