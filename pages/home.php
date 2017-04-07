9333c2ba965c3f8ca128c766df687323a13ddbf9
rohscx <rohscx@gmail.com>
2017-04-07 11:11:50 -0400

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,9 @@
 <p>Welcome to the <b>home</b> page.</p>
 <p>This is the initial release of the portal, updates, and changes will be included to increase functionality at a later date.</p>
 <p>The purpose of these tools is to provide insight into the operations of particular systems in a more easily absorbed format.</p>
+<p><b>Whats New!</b></p>
+<ul>
+  <li>A new Version has been relased! 17.0.<font color="red">2</font>!</li>
+  <li>ISE updated</li>
+  <li>PRIME updated</li>
+</ul>  	
