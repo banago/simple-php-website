73767fd87f02ad85faf9e452ce0b430fe82e7950
rohscx <rohscx@gmail.com>
2017-04-10 14:21:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
             font-size: 16px;
             color: white;
             text-align: center;
-            padding: 10px 16px;
+            padding: 16px 16px;
             text-decoration: none;
         }
         .dropdown {
