27548466b8a741c1f49ea41907eb80211e4086fa
rohscx <rohscx@gmail.com>
2017-04-10 13:05:33 -0400

    Update index.php

--- index.php
+++ index.php
@@ -118,7 +118,7 @@ function myFunction(d1) {
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown_1");
+    var myDropdown = document.getElementById("myDropdown_2");
       if (myDropdown.classList.contains('show')) {
         myDropdown.classList.remove('show');
       }
