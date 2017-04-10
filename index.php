85be1b222d2294c779b28b2421100e0dbd3668b6
rohscx <rohscx@gmail.com>
2017-04-10 12:25:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,65 @@
     <style type="text/css">
         .wrap { max-width: 700px; margin: 50px auto; padding: 30px; text-align: center; box-shadow: 0 0 5px rgba(0,0,0,.5); }
         article { text-align: left; padding: 40px; }
+        .container {
+    overflow: hidden;
+    background-color: #333;
+    font-family: Arial;
+}
+
+.container a {
+    float: left;
+    font-size: 16px;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+.dropdown {
+    float: left;
+    overflow: hidden;
+}
+
+.dropdown .dropbtn {
+    cursor: pointer;
+    font-size: 16px;    
+    border: none;
+    outline: none;
+    color: white;
+    padding: 14px 16px;
+    background-color: inherit;
+}
+
+.container a:hover, .dropdown:hover .dropbtn {
+    background-color: red;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+}
+
+.dropdown-content a {
+    float: none;
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+}
+
+.dropdown-content a:hover {
+    background-color: #ddd;
+}
+
+.show {
+    display: block;
+}
     </style>
 </head>
 <body>
@@ -15,12 +74,19 @@
     <header>
         <h2><?php siteName(); ?></h2>
         <nav class="menu">
-            <a href="/" title="Home">Home</a> |    
-            <a href="/?page=by-ise">ISE</a> |
-            <a href="/?page=by-prime">PRIME</a> |
-            <a href="/?page=by-dev-ise">Dev ISE</a> |
-            <a href="/?page=by-dev-prime">Dev PRIME</a> |
-            <a href="/?page=by-dev-apic">Dev APIC</a>
+            <div class="container">
+  <a href="/" title="Home">Home</a>
+  <a href="/?page=by-ise">ISE</a>
+  <a href="/?page=by-prime">PRIME</a>
+  <div class="dropdown">
+    <button class="dropbtn" onclick="myFunction()">Dropdown</button>
+    <div class="dropdown-content" id="myDropdown">
+      <a href="/?page=by-dev-ise">Dev ISE</a>
+      <a href="/?page=by-dev-prime">Dev PRIME</a>
+      <a href="/?page=by-dev-apic">Dev APIC</a>
+    </div>
+  </div> 
+</div>
         </nav>
     </header>    
 
@@ -31,5 +97,22 @@
 
     <footer><small>&copy;<?php echo date('Y'); ?> <?php siteName(); ?>. All rights reserved.<?php siteVersion(); ?></small></footer>
 </div>
+<script>
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
+</script>
 </body>
 </html>
