d7288045c1c351add1634a264a3a0937cf8cbe8e
rohscx <rohscx@gmail.com>
2017-04-10 12:30:35 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,64 +8,64 @@
         .wrap { max-width: 700px; margin: 50px auto; padding: 30px; text-align: center; box-shadow: 0 0 5px rgba(0,0,0,.5); }
         article { text-align: left; padding: 40px; }
         .container {
-    overflow: hidden;
-    background-color: #333;
-    font-family: Arial;
-}
+            overflow: hidden;
+            background-color: #333;
+            font-family: Arial;
+        }
 
-.container a {
-    float: left;
-    font-size: 16px;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-}
+        .container a {
+            float: left;
+            font-size: 16px;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
 
-.dropdown {
-    float: left;
-    overflow: hidden;
-}
+        .dropdown {
+            float: left;
+            overflow: hidden;
+        }
 
-.dropdown .dropbtn {
-    cursor: pointer;
-    font-size: 16px;    
-    border: none;
-    outline: none;
-    color: white;
-    padding: 14px 16px;
-    background-color: inherit;
-}
+        .dropdown .dropbtn {
+            cursor: pointer;
+            font-size: 16px;    
+            border: none;
+            outline: none;
+            color: white;
+            padding: 14px 16px;
+            background-color: inherit;
+        }
 
-.container a:hover, .dropdown:hover .dropbtn {
-    background-color: red;
-}
+        .container a:hover, .dropdown:hover .dropbtn {
+            background-color: red;
+        }
 
-.dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: #f9f9f9;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-}
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+        }
 
-.dropdown-content a {
-    float: none;
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-    text-align: left;
-}
+        .dropdown-content a {
+            float: none;
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+        }
 
-.dropdown-content a:hover {
-    background-color: #ddd;
-}
+        .dropdown-content a:hover {
+            background-color: #ddd;
+        }
 
-.show {
-    display: block;
-}
+        .show {
+            display: block;
+        }
     </style>
 </head>
 <body>
@@ -75,18 +75,18 @@
         <h2><?php siteName(); ?></h2>
         <nav class="menu">
             <div class="container">
-  <a href="/" title="Home">Home</a>
-  <a href="/?page=by-ise">ISE</a>
-  <a href="/?page=by-prime">PRIME</a>
-  <div class="dropdown">
-    <button class="dropbtn" onclick="myFunction()">Dropdown</button>
-    <div class="dropdown-content" id="myDropdown">
-      <a href="/?page=by-dev-ise">Dev ISE</a>
-      <a href="/?page=by-dev-prime">Dev PRIME</a>
-      <a href="/?page=by-dev-apic">Dev APIC</a>
-    </div>
-  </div> 
-</div>
+                <a href="/" title="Home">Home</a>
+                <a href="/?page=by-ise">ISE</a>
+                <a href="/?page=by-prime">PRIME</a>
+                <div class="dropdown">
+                    <button class="dropbtn" onclick="myFunction()">Dropdown</button>
+                    <div class="dropdown-content" id="myDropdown">
+                        <a href="/?page=by-dev-ise">Dev ISE</a>
+                        <a href="/?page=by-dev-prime">Dev PRIME</a>
+                        <a href="/?page=by-dev-apic">Dev APIC</a>    
+                    </div>  
+                </div> 
+            </div>
         </nav>
     </header>    
 
