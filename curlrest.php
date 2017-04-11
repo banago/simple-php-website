34a3317fafe4b792ab9dc485244690a3091091d1
rohscx <rohscx@gmail.com>
2017-04-11 11:51:21 -0400

    Update curlrest.php

--- curlrest.php
+++ curlrest.php
@@ -24,40 +24,44 @@ function myCurl($curlAuth, $curlData, $curlAddress) {
     } 
 }
 
-if (isset($_GET['primeData']) & isset($_GET['primeAddress'])) {
+if (isset($_GET['curlData']) & isset($_GET['curlAddress'])) {
+    $curlAuth = $apicAuth;
+    $curlData = $_GET['curlData'];
+    $curlAddress = $_GET['curlAddress'];       
+    $reponse = $myCurl($curlAuth, $curlData, $curlAddress)
     if ($array['http-code'] == 500) {
         echo print_r($array);
-    }
-} else {
-    $json = json_decode($response, true);
-    //print_r($json);  // debug
-    //echo $response;  // debug
-    //echo $json['vlanId']['associationTime']; // debug
-    $match = array("NAS Interface :"=>'clientInterface',"NAS Connection Type :"=>'connectionType',    
-                   "NAS IP :"=>'deviceIpAddress',"NAS Name :"=>'deviceName',                    
-                   "EndPoint Type :"=>'deviceType',"EndPoint IP :"=>'ipAddress',                      
-                   "EndPoint MAC :"=>'macAddress',"EndPoint NAC :"=>'securityPolicyStatus',
-                   "EndPoint OUI :"=>'vendor',"EndPoint VLAN:"=>'vlan');
-    //echo $json['queryResponse']['entity']['0']['clientsDTO']['securityPolicyStatus'] . "\r\n";   // debug
-    //echo print_r($json) . "\r\n";    // debug
-    if (isset($json['queryResponse']['entity'])) { 
-        for ($i = 0; $i < count($json['queryResponse']['entity']); $i++) {
-            //Debug
-            //echo "How many response: " . count($json['response']) . "<br>";
-            echo "<br>";
-            echo "Array Element: " . $i . "<br>";  
-            echo "<br>";    
-            foreach ($match as $x => $item) {
+    } else { 
+        $json = json_decode($response, true);
+        //print_r($json);  // debug
+        //echo $response;  // debug
+        //echo $json['vlanId']['associationTime']; // debug
+        $match = array("NAS Interface :"=>'clientInterface',"NAS Connection Type :"=>'connectionType',    
+                       "NAS IP :"=>'deviceIpAddress',"NAS Name :"=>'deviceName',                    
+                       "EndPoint Type :"=>'deviceType',"EndPoint IP :"=>'ipAddress',                      
+                       "EndPoint MAC :"=>'macAddress',"EndPoint NAC :"=>'securityPolicyStatus',
+                       "EndPoint OUI :"=>'vendor',"EndPoint VLAN:"=>'vlan');
+        //echo $json['queryResponse']['entity']['0']['clientsDTO']['securityPolicyStatus'] . "\r\n";   // debug
+        //echo print_r($json) . "\r\n";    // debug
+        if (isset($json['queryResponse']['entity'])) { 
+            for ($i = 0; $i < count($json['queryResponse']['entity']); $i++) {
+                //Debug
+                //echo "How many response: " . count($json['response']) . "<br>";
+                echo "<br>";
+                echo "Array Element: " . $i . "<br>";  
+                echo "<br>";    
+                foreach ($match as $x => $item) {
                     echo "<b>" . $x . "</b>" . "  " . $json['queryResponse']['entity']['0']['clientsDTO'][$item] . "<br>";    
-            } 
-        }   
-        echo "<p>" . "</p>";
-        echo "<p>" . "</p>";
-    } else {
-        echo "Unable to locate record for : " . "<font color=\"red\">" . $data . "</font>";
-        echo "<p>" . "</p>"; 
-        echo "<p>" . "</p>";
-    }  
-}   
+                } 
+        
+            }   
+            echo "<p>" . "</p>";
+            echo "<p>" . "</p>";
+        } else {
+            echo "Unable to locate record for : " . "<font color=\"red\">" . $data . "</font>";
+            echo "<p>" . "</p>"; 
+            echo "<p>" . "</p>";
+        }
+    }   
 }
 ?>
