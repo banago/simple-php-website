cc0a8416319d55b8d55325d3e08dc54f1ad1b734
rohscx <rohscx@gmail.com>
2017-04-11 15:04:18 -0400

    Update curlrest.php

--- curlrest.php
+++ curlrest.php
@@ -65,51 +65,5 @@ if (isset($_GET['curlData']) & isset($_GET['curlAddress'])) {
     }   
 }
 //  Deconstruct to create a new ticket getting function......
-function apicRest_1($ticket){ //function parameters, one variable
-	//Debug
-	//echo "Ticket : " . $ticket;
-	$curl = curl_init();
-	curl_setopt_array($curl, array(
-		CURLOPT_URL => "https://devnetapi.cisco.com/sandbox/apic_em/api/v1/network-device",
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_ENCODING => "",
-		CURLOPT_MAXREDIRS => 10,
-		CURLOPT_TIMEOUT => 30,
-		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		CURLOPT_CUSTOMREQUEST => "GET",
-		CURLOPT_POSTFIELDS => "{\"username\":\"devnetuser\",\n\"password\":\"Cisco123!\"\n}",
-		CURLOPT_HTTPHEADER => array(
-			"cache-control: no-cache",
-			"content-type: application/json",
-			"x-auth-token: " . $ticket
-			),
-		));
-	$response = curl_exec($curl);
-	$err = curl_error($curl);
-	curl_close($curl);
-	if ($err) {
-		echo "cURL Error #:" . $err;
-	} else {
-		//Debug
-		//echo $response;
-		$json = json_decode($response, true);
-		$match = array("Serial Number :"=>'serialNumber',"Family :"=>'family',"Type :"=>'type',
-			       "Inventory :"=>'inventoryStatusDetail',"MAC :"=>'macAddress',
-			       "Role :"=>'role',"MgmT :"=>'managementIpAddress',
-			       "Platform :"=>'platformId',"Reachablity :"=>'reachabilityStatus',
-			       "HostName :"=>'hostname');
-		for ($i = 0; $i < count($json['response']); $i++) {
-			//Debug
-			//echo "How many response: " . count($json['response']) . "<br>";
-			echo "<br>";
-			echo "Array Element: " . $i . "<br>";
-			echo "<br>";
-			foreach ($match as $x => $item) {
-				echo $x ."  " . $json['response'][$i][$item] . "<br>";
-			}
-		}
-		//Debug
-		//print_r($json);
-	}  
-}
+
 ?>
