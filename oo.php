e2b00f486bb2da0f19c19f257c01650f2167ee68
rohscx <rohscx@gmail.com>
2017-04-12 18:07:56 -0400

    Update oo.php

--- oo.php
+++ oo.php
@@ -1,2 +1,23 @@
+<?php
 class demo {
+
+  private $attribute1;
+
+  private $attribute2;
+
+  function operation1 () {
+
+  }
+
+  function operation2 () {
+
+  }
+
+  function __construct( ($param) {
+
+    echo "Constructor called with parameter ".$parm."<br />"; 
+
+  }
 }
+                      
+?>         
