60dcba6845d27c35cb4c289aabc48ee9e8a69ef5
rohscx <rohscx@gmail.com>
2017-04-12 16:20:11 -0400

    Update by-dev-prime.php

--- pages/by-dev-prime.php
+++ pages/by-dev-prime.php
@@ -289,7 +289,7 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+document.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
