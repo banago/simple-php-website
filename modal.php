1e70d5fbd7907808d55612a7edaac5e50e5f14f8
rohscx <rohscx@gmail.com>
2017-04-13 19:42:13 -0400

    Update modal.php

--- modal.php
+++ modal.php
@@ -1,5 +1,15 @@
 <?php
 class mymodal {
-  
+  function __construct($function) {
+	  $this->modal_1(); 
+  }
+  function __get($name){
+    return $this->$name;
+  }	// used to get properties
+  function __set($name,$value){
+    return $this->$name = $value;
+  }	// used to set properties
+  function modal_1(){
+  }
 }
 ?>
