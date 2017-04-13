5edb9f099d541453eeb6dbb76d936ae4f39dceb4
rohscx <rohscx@gmail.com>
2017-04-13 14:06:39 -0400

    Update curlrest.php

--- curlrest.php
+++ curlrest.php
@@ -210,7 +210,6 @@ if (isset($_GET['Type']) & isset($_GET['curlAddress']) & isset($_GET['curlData']
 	//echo $_GET['curlCustom'] . "<br />";	// debug
 	$a->curlPost = $_GET['curlPost'];
 	//echo $_GET['curlPost'] . "<br />";	// debug
-	
 	if ($_GET['Type'] == "primeTicket_1") {
 		$a->curlData = "(" . $_GET['curlData'] . ")";
 		$a->primeCurl_1();
