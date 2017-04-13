51756fbf5d43a090adf31bd6b85a68b9b357c7a1
rohscx <rohscx@gmail.com>
2017-04-13 10:10:55 -0400

    Update oo.php

--- oo.php
+++ oo.php
@@ -126,8 +126,8 @@ class curlauth {
 		$arr = array('serviceTicket' => $json['response']['serviceTicket'], 'idleTimeout' => $json['response']['idleTimeout'], 
 								 'sessionTimeout' => $json['response']['sessionTimeout'], 'sessionVersion' => $json['version']);	// create array for JSON
 		//echo json_encode($arr);		// return JSON
-		//$ticket = json_encode($arr);
-		$ticket = "Changed";
+		$ticket = json_encode($arr);
+		//$ticket = "Changed";
 		print_r($ticket);
 	}
 }
