e48abd6b7518cac001d542628f1e0b88ca9241de
rohscx <rohscx@gmail.com>
2017-04-14 14:44:53 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -165,14 +165,14 @@ if (isset($_GET['hostName_1'])){
 				//echo "PING RETURN    " . $match[1];	// debug
 				if ($match[1] == $arr['IPv4']) {
 					echo json_encode($arr);		// return JSON
-					//echo "PING AND DNS ARE EQUAL";	//debug
+					echo "PING AND DNS ARE EQUAL";	//debug
 				} elseif (empty($match[1])){
 					echo json_encode($arr);		// return JSON
-					//echo "PING FAILED, USING DNS VALUE";	// debug
+					echo "PING FAILED, USING DNS VALUE";	// debug
 				} else {
 				$arr = array('IPv4' => $match[1]);	 // create array for JSON
 				echo json_encode($arr);		// return JSON
-				//echo "PING AND DNS ARE NOT EQUAL USING PING VALUE"; // debug
+				echo "PING AND DNS ARE NOT EQUAL USING PING VALUE"; // debug
 				}
 			}
 		}
