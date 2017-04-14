d62518805ec86947d91e37648bab0914a420bce3
rohscx <rohscx@gmail.com>
2017-04-14 07:23:41 -0400

    Update by-dev-apic.php

--- pages/by-dev-apic.php
+++ pages/by-dev-apic.php
@@ -150,7 +150,7 @@ MAC | IP | HOSTNAME : <input type="text" name="data_text" id="uniqueID" onkeydow
 <!-- The Modal -->
 <div id="myModal" class="modal">
   <!-- Modal content -->
-  <div class="modal-content" >
+  <div class="modal-content" style="overflow-y: scroll; max-height:85%;  margin-top: 50px; margin-bottom:50px;">
     <div class="modal-header"> 
       <span class="close">&times;</span>
       <h2><center>APIC-EM GET Result</center></h2>
@@ -159,7 +159,7 @@ MAC | IP | HOSTNAME : <input type="text" name="data_text" id="uniqueID" onkeydow
       <p>LAST DETECTED ON</p>
         <div id="spinner" align="center" class="spinner"></div>
 	    <div style="text-align: center;">
-		    <div id="adiv2" class="pagination" style="display: inline-block; text-align: left" style="overflow-y: scroll; max-height:85%;  margin-top: 50px; margin-bottom:50px;">
+		    <div id="adiv2" class="pagination" style="display: inline-block; text-align: left">
 			    Content<br /> style="font-size:20px">
 
 		    </div>	    
