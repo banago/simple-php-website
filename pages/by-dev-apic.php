45ac3ff593e4bbad3a46bd410a7039153a0eb8ca
rohscx <rohscx@gmail.com>
2017-04-14 09:52:35 -0400

    Update by-dev-apic.php

--- pages/by-dev-apic.php
+++ pages/by-dev-apic.php
@@ -44,7 +44,7 @@ function encoded_1(thediv, thefile, thekey) {
 		    document.getElementById(thediv).innerHTML = ""; 	//clears the div
 		    apicreturn_1(thediv, 'curlrest.php' , 'Type' , 'apicTicket_1' , 'curlAddress', myUrl , 'curlData' , myObj.Encoded , 'curlCustom' , 'GET' , 'curlPost' , '');
 	    } else if (myObj.Type == "HostName") {
-		    if (myObj.Encoded.length >=5 ){
+		    if (myObj.Encoded.length >=100 ){
 			    myUrl = encodeURIComponent("https://agaprimepr01.fpicore.fpir.pvt/webacs/api/v1/data/Clients.json?.full=true\&ipAddress=eq");
 			    var a = document.getElementById(thediv).innerHTML = ""; 	//clears the div
 			    apicreturn_1(thediv, 'curlrest.php' , 'Type' , 'apicTicket_1' , 'curlAddress', myUrl , 'curlData' , myObj.Encoded , 'curlCustom' , 'GET' , 'curlPost' , ''); 
