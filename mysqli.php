339c2a0e2594145497d747455423c8485aa9c7bc
rohscx <rohscx@gmail.com>
2017-04-17 16:38:08 -0400

    Update mysqli.php

--- mysqli.php
+++ mysqli.php
@@ -3,22 +3,26 @@ header('Content-Type: text/html; charset=utf-8');
 class mysqlquery {
 	protected $mac_1;
   	protected $int_1;
+	protected $db_s_1 = 'sql'; 	// db server dns name
+	protected $db_su_1 = 'demoUser';	// db username
+	protected $db_sp_1 = 'demoPassword';	// db password
+	protected $db_sd_1 = 'MAB_TRACK';	// db database name
 	protected $serchtype_1 = "Valid_Until";
 	protected $searchterm_1;
 	protected $query_1 = "SELECT Mac_ID, Valid_From, Valid_Until, Aca_ID, User_ID , State FROM aca_mab WHERE Valid_Until = ?";
 	protected $resultrow = array();
-  	function __construct($sqlQuery,$sqlSearchterm) {
+  	function __construct($sqlQuery,$sqlWhere) {
 	  if ($sqlQuery == "query_1") {
-		  $this->sqlquery($this->query_1, $sqlSearchterm);
+		  $this->sqlquery($this->query_1, $sqlWhere);
 	  } elseif ($function == "primeTicket_1") {
 		  $this->primeTicket_1();
 	  } elseif ($function == "iseTicket_1") {
 		  $this->iseTicket_1();
 	  }	
   }
-   function sqlquery($Query, $searchTerm) {
-	  $this->searchterm_1 = $searchTerm;
-	  $db = new mysqli('sql', 'demoUser', 'demoPassword', 'MAB_TRACK');
+   function sqlquery($Query, $sqlWhere) {
+	  $this->searchterm_1 = $sqlWhere;
+	  $db = new mysqli($this->db_s_1, $this->db_su_1, $this->db_sp_1, $this->db_sd_1);
  	  $stmt = $db->prepare($Query);
 	  $stmt->bind_param('s', $this->searchterm_1);
 	  $stmt->execute();
@@ -57,8 +61,8 @@ class mysqlquery {
 }
 
 //$db = new mysqlquery("1000-01-01 00:00:0");
-if (isset($_GET['sqlQuery']) & isset($_GET['sqlData'])) {
-	$db = new mysqlquery($_GET['sqlQuery'], $_GET['sqlData']);	// sets class property
+if (isset($_GET['sqlQuery']) & isset($_GET['sqlWhere'])) {
+	$db = new mysqlquery($_GET['sqlQuery'], $_GET['sqlWhere']);	// sets class property
 	}	
 }
 ?>
