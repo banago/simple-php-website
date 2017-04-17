1b7a1362fc9cd50a9bb1faf67e915ee6ff7bb661
rohscx <rohscx@gmail.com>
2017-04-17 15:52:14 -0400

    Update mysqli.php

--- mysqli.php
+++ mysqli.php
@@ -7,22 +7,23 @@ class mysqlquery {
 	protected $searchterm_1;
 	protected $query_1 = "SELECT Mac_ID, Valid_From, Valid_Until, Aca_ID, User_ID , State FROM aca_mab WHERE Valid_Until = ?";
 	protected $resultrow = array();
-  	function __construct($sqlquery) {
-	  if ($function == "sqlMAC") {
-		  $this->mac2int_1();
+  	function __construct($sqlQuery,$sqlSearchterm) {
+	  if ($sqlQuery == "query_1") {
+		  $this->sqlquery($this->query_1, $sqlSearchterm);
 	  } elseif ($function == "primeTicket_1") {
 		  $this->primeTicket_1();
 	  } elseif ($function == "iseTicket_1") {
 		  $this->iseTicket_1();
 	  }
-	  $this->sqlquery($sqlquery);
+	  if ()
+	  
 
 		
   }
-   function sqlquery($sqlquery) {
-	  $this->searchterm_1 = $sqlquery;
+   function sqlquery($Query, $searchTerm) {
+	  $this->searchterm_1 = $searchTerm;
 	  $db = new mysqli('sql', 'demoUser', 'demoPassword', 'MAB_TRACK');
- 	  $stmt = $db->prepare($this->query_1);
+ 	  $stmt = $db->prepare($Query);
 	  $stmt->bind_param('s', $this->searchterm_1);
 	  $stmt->execute();
 	  $stmt->store_result();
@@ -60,8 +61,8 @@ class mysqlquery {
 }
 
 //$db = new mysqlquery("1000-01-01 00:00:0");
-if (isset($_GET['sqlData'])) {
-	$db = new mysqlquery($_GET['sqlData']);	// sets class property
+if (isset($_GET['sqlQuery']) & isset($_GET['sqlData'])) {
+	$db = new mysqlquery($_GET['sqlQuery'], $_GET['sqlData']);	// sets class property
 	//echo $_GET['Type'] . "<br />";	// debug
 	//$a->curlAddress = $_GET['curlAddress'];	// sets class property
 	//echo $_GET['curlAddress'] . "<br />";	// debug
