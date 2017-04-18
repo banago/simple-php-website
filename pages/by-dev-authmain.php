219cc38b5668acefaa76a3b28c52ac55e3247eee
rohscx <rohscx@gmail.com>
2017-04-18 14:39:12 -0400

    Update by-dev-authmain.php

--- pages/by-dev-authmain.php
+++ pages/by-dev-authmain.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST['userid']) && isset ($_POST['password'])) {
   // if the user has justed tried to log in
-  $userid = strtoupper($_POST['userid']);
+  //$userid = strtoupper($_POST['userid']);
   $password = $_POST['password'];
   
   $db_conn = new mysqli('sql', 'demoUser', 'demoPassword','MAB_TRACK');
