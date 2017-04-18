f8b4d968ab529f2b5dbce5131b6b8cd9413e9c26
rohscx <rohscx@gmail.com>
2017-04-18 11:06:42 -0400

    Update authmain.php

--- authmain.php
+++ authmain.php
@@ -10,7 +10,7 @@ if (isset($_POST['userid']) && isset ($_POST['password'])) {
     echo 'Connection to database failed:' . mysqli_connect_error();
     exit();
   }
-  $query = "SELECT * FROM authorized_users WHERE name = '" . $userid . "' AND password=sha1('".$password."')";
+  $query = "SELECT aca_user.Fname, aca_user.Fname, aca_user.User_ID, aca_user_password.Password FROM aca_user, aca_user_password WHERE name = '" . $userid . "' AND password=sha1('".$password."')";
   $result = $db_conn->query($query);
   if ($result->num_rows) {
     // if they are in the database register teh user id
