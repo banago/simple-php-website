8efa420a924e07a10efe1fef170fa4c10804bb87
rohscx <rohscx@gmail.com>
2017-04-18 16:35:42 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -1,6 +1,6 @@
 <?php
 if (!isset($_SESSION['valid_user'])) {
-    echo "This is a secured page please login to continue"
+    echo "This is a secured page please login to continue";
     echo "<p><a href=\"?page=by-dev-authmain\">ISE Bypass Login Page</a></p>";
 } 
 ?>
