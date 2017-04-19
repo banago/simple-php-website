c32ca713c58aafa4d6022ce5a8bafed1df9bb70d
rohscx <rohscx@gmail.com>
2017-04-19 11:20:58 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -1,9 +1,4 @@
-<?php if (!isset($_SESSION['valid_user'])) : ?>
-    This is a secured page please login to continue <br />
-    <p><a href="?page=by-dev-authmain">ISE Bypass Login Page</a></p>
-<?php endif; ?>
-
-<?php if ($_SESSION['timeout_idle'] < time()) : ?>
+<?php if ($_SESSION['timeout_idle'] < time() || !isset($_SESSION['valid_user'])) : ?>
     This is a secured page please login to continue <br />
     <p><a href="?page=by-dev-authmain">ISE Bypass Login Page</a></p>
 <?php endif; ?>
