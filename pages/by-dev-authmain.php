502d346f853aadbcc1579757d4b9df81343caf19
rohscx <rohscx@gmail.com>
2017-04-18 22:46:48 -0400

    Update by-dev-authmain.php

--- pages/by-dev-authmain.php
+++ pages/by-dev-authmain.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST['userid']) && isset($_POST['password'])) {
   // if the user has justed tried to log in
-  $userid = strtoupper($_POST['userid']);
+  $userid = strtoupper($_POST['userid']); // makes user name uppercase
   $password = $_POST['password'];
   
   $db_conn = new mysqli('sql', 'demoUser', 'demoPassword','MAB_TRACK');
@@ -11,20 +11,21 @@ if (isset($_POST['userid']) && isset($_POST['password'])) {
   }
   $query = "SELECT au.Fname, au.Fname, au.User_ID, aup.Password 
   FROM aca_user as au, aca_user_password as aup 
-  WHERE au.Fname = '" . $userid . "' AND au.Type = 'ADMINISTRATOR' AND au.User_ID = aup.User_ID AND aup.Password=sha1('".$password."')";
-  $result = $db_conn->query($query);
+  WHERE au.Fname = '" . $userid . "' AND au.Type = 'ADMINISTRATOR' AND au.User_ID = aup.User_ID AND 
+  aup.Password=sha1('".$password."')";
+  $result = $db_conn->query($query);  // executes query
   if ($result->num_rows) {
     // if they are in the database register the user id
-    $row = $result->fetch_assoc();
+    $row = $result->fetch_assoc();  // stores result of successfull query
     if ($row['Fname'] == $userid) {
-      $_SESSION['valid_user'] = $userid;
+      $_SESSION['valid_user'] = $userid;  // sets session to returned username
     } else {
       echo "UserName <font color=\"red\"><b>" . $userid . "</b> </font>" . " <ins>Not Found</ins>";
     }
   } else {
-    echo "THIS IS THE QUERY    :" . $query; // debug
+    //echo "THIS IS THE QUERY    :" . $query; // debug
   }
-  $db_conn->close();
+  $db_conn->close();  // closes the db connection
 }
 ?>
 <!DOCTYPE html>
@@ -35,7 +36,7 @@ if (isset($_POST['userid']) && isset($_POST['password'])) {
     </head>
   <body>
     <h1> Authentication Required</h1>
-    <?php
+    <?php // there is a better way to write this, rewrite it...
     if (isset($_SESSION['valid_user'])) {
     echo '<p>You are logged in as: '. $_SESSION['valid_user'] . ' <br />';
     echo '<a href="/?page=by-dev-logout">Log Out</a></p>';
