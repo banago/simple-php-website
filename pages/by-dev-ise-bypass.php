ddde4b541abdf49be4c3ff81d7fbf47f67d67fc9
rohscx <rohscx@gmail.com>
2017-04-22 02:46:12 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -111,7 +111,7 @@ function default_list(thediv, thefile, thekeyA_1, thekeyB_1, thekeyA_2, thekeyB_
 				for (i1 = 0; i1 < myObj.length; i1++) {
 					var newdiv = document.createElement('div');
 				    	newdiv.className = 'flex-item';
-					newdiv.backgroundColor = "PaleGreen";
+					newdiv.backgroundColor = 'PaleGreen';
 				    	newdiv.innerHTML = myObj[i1].Mac_ID+' '+myObj[i1].Fname+' '+myObj[i1].Lname+' '+myObj[i1].Valid_From+' '+myObj[i1].State;
 				    	document.getElementById(thediv).appendChild(newdiv);  
 				}   
