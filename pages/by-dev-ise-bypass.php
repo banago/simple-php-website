99b6010cf45b13ed8aa1c7f163299dfd2a7df1f8
rohscx <rohscx@gmail.com>
2017-04-22 01:05:20 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -111,7 +111,7 @@ function b(thediv, thefile, thekeyA_1, thekeyB_1, thekeyA_2, thekeyB_2) {
 		    document.getElementById('testdiv').innerHTML = myObj.length;
 		    document.getElementById('testdiv').innerHTML = myObj[0].Mac_ID;
 		    if (myObj[0].hasOwnProperty("Mac_ID")) {
-			    dvar myNode = document.getElementById('flex_div_1');
+			    var myNode = document.getElementById('flex_div_1');
 
 			    while (myNode.firstChild) {
     
