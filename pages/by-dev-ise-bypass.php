8813a8c981eabb21e7e38b0bc955ccdad4346cd8
rohscx <rohscx@gmail.com>
2017-04-22 00:31:02 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -110,7 +110,13 @@ function b(thediv, thefile, thekeyA_1, thekeyB_1, thekeyA_2, thekeyB_2) {
 		    document.getElementById('testdiv').innerHTML = myObj.length;
 		    document.getElementById('testdiv').innerHTML = myObj[0].Mac_ID;
 		    if (myObj[0].hasOwnProperty("Mac_ID")) {
-			    document.getElementById('testdiv').innerHTML = this.responseText;
+			    
+			    for (i1 = 0; i1 < myObj.Mac_ID.length; i1++) {
+				    for (i2 = 0; i2 < myObj.Mac_ID[i1].length; i2++){
+					    document.getElementById('testdiv').innerHTML = this.responseText;
+					    
+				    }
+			    }
 			    
 		    }
 	    }
