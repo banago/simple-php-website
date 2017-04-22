d191b63ec7ebd5e08d8fbaf5d2cf5cea327ed048
rohscx <rohscx@gmail.com>
2017-04-22 01:43:33 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -93,7 +93,7 @@ function encoded_1(thediv, thefile, thekey) {
 xmlhttp.open('GET', thefile+'?'+thekey+'='+document.search.data_text.value, true);
 xmlhttp.send();
 }
-function b(thediv, thefile, thekeyA_1, thekeyB_1, thekeyA_2, thekeyB_2) {    
+function default_list(thediv, thefile, thekeyA_1, thekeyB_1, thekeyA_2, thekeyB_2) {    
 	if (window.XMLHttpRequest) {
         xmlhttp = new XMLHttpRequest();	    
 	} else { 
@@ -223,9 +223,9 @@ var span = document.getElementsByClassName("close")[0];
 var rst = document.getElementById("myRst");
 // When the user clicks the button, open the modal
 //var formvalue_1 = document.getElementById("uniqueID").value;	
-	var cats = encodeURIComponent("1000-01-01 00:00:0");
+default_list('flex_div_1','mysqli.php','sqlQuery','query_3','sqlWhere',cats);
 btn.onclick = function() {
-    document.getElementById("adiv2").innerHTML = b('flex_div_1','mysqli.php','sqlQuery','query_3','sqlWhere',cats);
+    //document.getElementById("adiv2").innerHTML = b('flex_div_1','mysqli.php','sqlQuery','query_3','sqlWhere',cats);
     //document.getElementById("adiv2").innerHTML = restmodal('adiv2','restAuth.php','get_ticket');
     document.getElementById("adiv2").innerHTML = encoded_1('adiv2','functions.php','data_2');
     //document.getElementById("adiv2").innerHTML = document.getElementById("uniqueID").value;
