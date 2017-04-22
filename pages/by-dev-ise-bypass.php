28239c71f59983b4288e805a5231c524bf47d1aa
rohscx <rohscx@gmail.com>
2017-04-21 20:46:58 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -104,7 +104,7 @@ function encoded_2(thediv, thefile, thekeyA_1, thekeyB_1, thekeyA_2, thekeyB_2) 
             myObj = JSON.parse(this.responseText);
 	    var div = document.createElement('div');
 	    div.className = 'flex-item';
-	    document.getElementById('testdiv').innerHTML = myObj.[0].Mac_ID;
+	    document.getElementById('testdiv').innerHTML = myObj.Mac_ID;
 	    if (myObj.hasOwnProperty("Mac_ID")) {
 		    //myUrl = encodeURIComponent("https://agaisepr01.fpicore.fpir.pvt/admin/API/mnt/Session/MACAddress/");
 		    //document.getElementById('spinner').style.display = "none";
