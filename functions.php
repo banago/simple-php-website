f5c4ad8cbf5e43d549aa81a351cd0406172328e2
rohscx <rohscx@gmail.com>
2017-04-24 16:10:38 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -180,7 +180,7 @@ if (isset($_GET['hostName_1'])){
 					//echo "PING AND DNS ARE NOT EQUAL USING PING VALUE"; // debug
 					break;
 				} 
-			} else if ($ip == $host . $suffix || $ip="") {
+			} else if ($ip == $host . $suffix) {
 			  $arr = array('Failure' => $failure);	 // create array for JSON
 			  echo json_encode($arr);		// return JSON
 			  break;
