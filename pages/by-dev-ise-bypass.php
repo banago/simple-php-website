9707dfe2637bcb61631bfc90e06be396195d6aca
rohscx <rohscx@gmail.com>
2017-04-23 21:31:41 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -230,7 +230,7 @@ function curlreturn_1(thediv, thefile  , thetype , thetypeval , thekey_1 , theva
 	    <h2><center>PRIME GET Result</center></h2>   
 	  </div>    
 	  <div class="modal-body" align="center">      
-	    <p>LAST DETECTED ON</p>        
+	    <p>RESULT</p>        
 	    <div id="spinner" align="center" class="spinner"></div>
 	    <div style="text-align: center;">
 		    <div id="adiv2" class="apicdata" style="display: inline-block; text-align: left">
