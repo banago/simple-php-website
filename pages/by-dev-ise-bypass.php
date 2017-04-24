3a27ff88b28e82315379f62fffbe47b1e21f4968
rohscx <rohscx@gmail.com>
2017-04-23 21:30:16 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -143,6 +143,7 @@ function get_notes(thediv, thefile, thekeyA_1, thekeyB_1, thekeyA_2, thekeyB_2) 
 		if (this.readyState == 4 && this.status == 200) {
 			myObj = JSON.parse(this.responseText);
 		    	if (myObj[0].hasOwnProperty("Mac_ID")) {
+				document.getElementById('spinner').style.display = "none";
 				document.getElementById("adiv2").innerHTML = "MAC : "+myObj[0].Mac_ID+'<br />'+'<p>'+myObj[0].Note+'</p>';
 			}
 		}
