66c104add63808d1002e744a32e3a266b5ca618a
rohscx <rohscx@gmail.com>
2017-04-24 18:12:43 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -15,7 +15,7 @@
 	    <div class="flex-content">
 		<div class="flex-header">
 		    <h2><center>BYPASS LIST</center></h2>
-			<div id="newsearch" align="left"><input type="text" name="search" placeholder="Search.."></div>
+			<div id="search_1" align="left"><form><input type="text" name="search" placeholder="Search.."></form></div>
 		</div>             
 		<div class="flex-body" id="flex_div_1">	
 		    <div class="flex-item">{01:23:45:67:89:ab} Demo-User-Name Demo-User-Status</div>
