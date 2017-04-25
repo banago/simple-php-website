a19fe7c3155d0a3a438e74711f708903db69a198
rohscx <rohscx@gmail.com>
2017-04-25 16:00:00 -0400

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -2,11 +2,11 @@
 <p>This is the initial release of the portal, updates, and changes will be included to increase functionality at a later date.</p>
 <p><b>Whats New!</b></p>
 <ul>
-  <li>A new Version has been relased! 17.0.<font color="red">3</font>!</li>
+  <li>A new Version has been relased! 17.0.<font color="red">5</font>!</li>
+  <li>General UI Changes</li>
+  <li>ISE BYPASS page added</li>
+  <li>Authentication page added</li>
   <li>More robust hostname looks comparing DNS and PING results</li>
   <li>All REST interactions are now completed through an Object</li>
-  <li>ISE updated</li>
-  <li>PRIME updated</li>
-  <li>Menu Bar updated</li>
 </ul>  	
 <p>The purpose of these tools is to provide insight into the operations of particular systems in a more easily absorbed format.</p>
