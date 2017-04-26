2b46759562007abd63202c6765e4504e74723120
rohscx <rohscx@gmail.com>
2017-04-26 17:39:00 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -417,5 +417,10 @@ document.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 </script>
 <?php endif; ?>
