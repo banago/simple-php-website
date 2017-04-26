96b26190007d4a6aab423647729b26a9dd38f25b
rohscx <rohscx@gmail.com>
2017-04-26 14:13:39 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -259,26 +259,26 @@ function curlreturn_1(thediv, thefile  , thetype , thetypeval , thekey_1 , theva
 
 <div id="id01" class="modal">
   
-  <form class="modal-content animate" action="/action_page.php">
-    <div class="imgcontainer">
+  <form id="id01" class="modal-content animate" action="/action_page.php">
+    <div id="id01" class="imgcontainer">
       <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
       <img src="img_avatar2.png" alt="Avatar" class="avatar">
     </div>
 
-    <div class="container">
+    <div id="id01" class="container">
       <label><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
+      <input id="id01" type="text" placeholder="Enter Username" name="uname" required>
 
       <label><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
+      <input id="id01" type="password" placeholder="Enter Password" name="psw" required>
         
-      <button type="submit">Login</button>
-      <input type="checkbox" checked="checked"> Remember me
+      <button id="id01" type="submit">Login</button>
+      <input id="id01" type="checkbox" checked="checked"> Remember me
     </div>
 
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
+    <div id="id01" class="container" style="background-color:#f1f1f1">
+      <button id="id01" type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <span id="id01" class="psw">Forgot <a href="#">password?</a></span>
     </div>
   </form>
 </div>
