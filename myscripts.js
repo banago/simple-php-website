22caa2420c934112b59d3034afe0f48987b87868
rohscx <rohscx@gmail.com>
2017-04-25 20:55:44 -0400

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,37 @@
-
+function sqli_interface (thediv,thefile,thekey,thedocument) {
+  this.thediv = thediv;
+  this.thefile = thefile;
+  this.theykey = thekey;
+  this.thedocument = thedocument;
+  
+  
+  
+  function dbsearch_1(this.thediv, this.thefile, this.thekey, this.thedocument) {
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else { 
+        xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+    }  
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            myObj = JSON.parse(this.responseText);
+            document.getElementById(this.thediv).innerHTML =  myObj.Type.fontcolor("green")  + " : " + myObj.Normalized + "<br>" + myObj.Encoded;	// debug
+	    if (myObj.Type == "HostName"){
+		    var str = myObj.Normalized;
+		    var res = str.replace(/"/g, "");	// strips exclimation ponts
+		    document.getElementById(this.thediv).innerHTML = "HostName "+res;	// debug
+		    default_list('flex_div_1','mysqli.php','sqlQuery','query_5','sqlWhere',res);   
+	    } else if (myObj.Type == "MAC"){
+		    var str = myObj.Normalized;
+		    var res = encodeURIComponent(str);
+		    document.getElementById(this.thediv).innerHTML = "MAC "+res;	// debug
+		    default_list('flex_div_1','mysqli.php','sqlQuery','query_6','sqlWhere',res);
+	    } else {
+		    default_list('flex_div_1','mysqli.php','sqlQuery','query_3','sqlWhere',encodeURIComponent("1000-01-01 00:00:0"));
+	    }
+        }
+    }
+    xmlhttp.open('GET', this.thefile+'?'+this.thekey+'='+document.search_2.data_text.value, true);  // add this.thedocument inplace of the document
+    xmlhttp.send();
+  }
+}
