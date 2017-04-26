1ca10b944b0ec3acfea92c1a24a478c917f4359a
rohscx <rohscx@gmail.com>
2017-04-26 08:37:58 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -223,6 +223,10 @@ function curlreturn_1(thediv, thefile  , thetype , thetypeval , thekey_1 , theva
     xmlhttp.open('GET', thefile+'?'+thetype+'='+thetypeval+'&'+thekey_1+'='+theval_1+'&'+thekey_2+'='+theval_2+'&'+thekey_3+'='+theval_3+'&'+thekey_4+'='+theval_4, true);
     	xmlhttp.send();	
 }
+			window.onload = function() {
+   		<script src="myscripts.js">
+</script>
+}
 	</script>
 
 </head>
@@ -285,8 +289,10 @@ function curlreturn_1(thediv, thefile  , thetype , thetypeval , thekey_1 , theva
 	    </div>  
 	  </div>
 	</div>
-<script src="myscripts.js">
-</script>
+
+
+		
+
 <body/>
 <footer>
 
