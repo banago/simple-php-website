98af8116f857061d8134cefb5d72747e7638ebb0
rohscx <rohscx@gmail.com>
2017-04-26 16:13:56 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -282,7 +282,34 @@ function curlreturn_1(thediv, thefile  , thetype , thetypeval , thekey_1 , theva
 	    </div>  
 	  </div>
 	</div>
+<div data-role="page">
+  <div data-role="header">
+    <h1>Welcome To My Homepage</h1>
+  </div>
 
+  <div data-role="main" class="ui-content">
+    <a href="#myPopup" data-rel="popup" class="ui-btn ui-btn-inline ui-corner-all ui-icon-check ui-btn-icon-left">Show Popup Form</a>
+
+    <div data-role="popup" id="myPopup" class="ui-content" style="min-width:250px;">
+      <form method="post" action="/action_page_post.php">
+        <div>
+          <h3>Login information</h3>
+          <label for="usrnm" class="ui-hidden-accessible">Username:</label>
+          <input type="text" name="user" id="usrnm" placeholder="Username">
+          <label for="pswd" class="ui-hidden-accessible">Password:</label>
+          <input type="password" name="passw" id="pswd" placeholder="Password">
+          <label for="log">Keep me logged in</label>
+          <input type="checkbox" name="login" id="log" value="1" data-mini="true">
+          <input type="submit" data-inline="true" value="Log in">
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <div data-role="footer">
+    <h1>Footer Text</h1>
+  </div>
+</div> 
 <script>
 // Get the modal
 var modal = document.getElementById('myModal');
