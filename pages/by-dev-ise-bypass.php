e4f3ba958599c8ac9f31a3ba96a2a30afd7d241b
rohscx <rohscx@gmail.com>
2017-04-27 08:48:13 -0400

    Update by-dev-ise-bypass.php

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -317,10 +317,7 @@ button:hover {
       <input type="checkbox" checked="checked"> Remember me
     </div>
 
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
-    </div>
+ 
   </form>
 </div>
 
