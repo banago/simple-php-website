9755b368ce9e6d956ef1b2d57f1fd96ecc766586
Baki Goxhaj <banago@gmail.com>
2017-05-02 15:29:37 +0200

    Check if config key is set.

--- config.php
+++ config.php
@@ -8,24 +8,18 @@
 function config($key = '')
 {
     $config = [
-
         'name' => 'Simple PHP Website',
-
         'nav_menu' => [
             '' => 'Home',
             'about-us' => 'About Us',
             'products' => 'Products',
             'contact' => 'Contact',
         ],
-
         'template_path' => 'template',
-
         'content_path' => 'content',
-
         'pretty_uri' => true,
-
         'version' => 'v2.0',
     ];
 
-    return $config[$key] ?: null;
+    return isset($config[$key]) ? $config[$key] : null;
 }
