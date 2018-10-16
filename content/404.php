307e45e6f9d39cf16e0d3c13d3b230d4e2c80e52
Harsha Deepa <44192755+harshadeepa@users.noreply.github.com>
2018-10-16 15:09:40 +0530

    Update 404.php
    I've improved the ERROR (404) page, with some html & added a title header to it, so that it looks better!

--- content/404.php
+++ content/404.php
@@ -1,1 +1,9 @@
-<p>404 - This page does not exist.</p>
+<html>
+<head>
+<title> 404 Not Found! </title>
+  </head>
+<body>
+<center><p1>Error (404) - This Page Does Not Exist!</p><center>
+  <marquee><p3> This page does not exist, or can't be found </p></marquee>
+  </body>
+</html>
