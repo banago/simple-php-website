4c6de4c69581137df4e354158ae3861b6dfe94bf
zafims <55727089+zafims@users.noreply.github.com>
2020-09-09 01:41:17 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 // Comment these lines to hide errors
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
+//This comment is done by Zafar Imam Munna
+//I made this comment for Assignment purpose
 require 'includes/config.php';
 require 'includes/functions.php';
 
