62caae08382fc81a1600294477dce570151e2f82
Lutfar1996 <63013110+Lutfar1996@users.noreply.github.com>
2020-09-14 11:03:17 +0600

    commit change

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 // Comment these lines to hide errors
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-ini_set('display_errors',2);
+ini_set('display_errors',3);
 
 require 'includes/config.php';
 require 'includes/functions.php';
