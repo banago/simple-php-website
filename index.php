3f8a7c5ee8b04861fec8576634910dcfb06e8558
Lutfar1996 <lutfar.rahman569@gmail.com>
2020-09-14 11:31:12 +0600

    testing commits

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_errors',3);
-
+ini_set('display_errors',4);
 require 'includes/config.php';
 require 'includes/functions.php';
 
