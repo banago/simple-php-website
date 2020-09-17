6d2a3bfc5e513466027c607757783d57185fa713
Engr. Zafar <55727089+zafims@users.noreply.github.com>
2020-09-17 20:09:54 +0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ ini_set('display_errors', 0);
 
 require 'includes/config.php';
 require 'includes/functions.php';
-
+//This Change is made by Zafar
+//Assignment 01 by Zafar
 //Changed by Tanvir_Assignment_01
 
 init();
