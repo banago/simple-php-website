8af1e46cf65ea5c30fef60e1ea37d510f8b30c78
Satvik Mittal <satvikmittal638@gmail.com>
2021-10-07 09:31:38 +0530

    Commented out lines in Index.php to hide errors

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 // Comment these lines to hide errors
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 
 require 'includes/config.php';
 require 'includes/functions.php';
