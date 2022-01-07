293c904978d0f96a2f5bf245334b62e6e0a8dea3
Um-esh <92661877+Um-esh@users.noreply.github.com>
2022-01-07 05:38:20 +0000

    Add files via upload

---
+++ Con.php
@@ -0,0 +1,9 @@
+<div id='slideshow'>
+  <div class='slide-wrapper'>
+    <div class='slide'><h1 class='slide-number'>1</h1></div>
+    <div class='slide'><h1 class='slide-number'>2</h1></div>
+    <div class='slide'><h1 class='slide-number'>3</h1></div>
+    <div class='slide'><h1 class='slide-number'>4</h1></div>
+    <div class='slide'><h1 class='slide-number'>5</h1></div>
+  </div>
+</div>
